dac30134cda22bc599617054f46d1d395d828fd2
Michele Lindroos <michele.lindroos@omoroi.fi>
2020-11-14 12:53:37 +0200

    add dotfiles.sh

---
+++ dotfiles.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+FILES=(
+    .tmux.conf
+    .vimrc
+    )
+
+function run_diff_all {
+    echo "Not implemented :-)"
+}
+
+function run_diff {
+    file=$1
+
+    if [ -z "${file}" ]; then
+        run_diff_all
+    else
+        file_source=${file}
+        file_destination=${HOME}/${file}
+        echo "Showing diff between ${file_source} and ${file_destination}"
+        diff -u "${file_source}" "${file_destination}"
+    fi
+}
+
+function list_diffs {
+    for file in "${FILES[@]}"; do
+        file_source=${file}
+        file_destination=${HOME}/${file}
+        if [ -f "${file_destination}" ]; then
+            diff -q "${file_source}" "${file_destination}"
+        else
+            echo "${file}: ${file_destination} does not exist!"
+        fi
+    done
+}
+
+function run_install_all {
+    echo "Not implemented :-)"
+}
+
+function run_install {
+    file=$1
+
+    if [ -z "${file}" ]; then
+        run_install_all
+    else
+        file_source=${file}
+        file_destination=${HOME}/${file}
+        echo "Installing ${file_source} to ${file_destination}..."
+        cp "${file_source}" "${file_destination}"
+        echo "Done!"
+    fi
+}
+
+runcommand=$1
+file=$2
+
+case "${runcommand}" in
+    diff)
+        run_diff "${file}"
+        ;;
+    list)
+        list_diffs
+        ;;
+    install)
+        run_install "${file}"
+        ;;
+esac
+
